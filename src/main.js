75a3b6b7f4e77d5c5b759cb273ba53d388cfc623
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-27 22:48:29 +0530

    Promise rejected state

--- src/main.js
+++ src/main.js
@@ -25,8 +25,25 @@ myPromise.then(value => {
 });
 
 
+//----rejected-----------//
+const myPromise1 = new Promise((resolve, reject) => {
+    const error = true;
+    if (!error) {
+        resolve("Yes! resolved the Promise!");
+    }else {
+        reject("No! rejected the Promise!");
+    }
+});
 
-
+console.log(myPromise1);
+//----this operation will just skips all the code if there is an error is present---//
+myPromise1.then(value => {
+    return value + 1111;
+}).then(newValue => {
+    console.log(newValue);
+}).catch(error => {
+    console.error(error);
+});
 
 
 
